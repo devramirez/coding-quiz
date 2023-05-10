ae901aa5469a3202e5fc2501d01cbdb60fc2d4dd
devramirez <aramirezdev1@gmail.com>
2023-05-10 18:10:31 -0400

    added questions section and button for questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
-// setting a timer interval
-var timer = 60;
+var count = 0;
+var startEl = document.querySelector('.start-btn');
+var countEl = document.querySelector('.count');
+
+// Function to countdown
+function countDown() {
+    var timer = 60;
 setInterval(() => {
   document.getElementById("timer").textContent = timer;
   if (timer > 0) {
     timer--;
-    console.log(timer);
   }
 }, 1000);
+}
+
+// setting a timer interval
+// var timer = 60;
+// setInterval(() => {
+//   document.getElementById("timer").textContent = timer;
+//   if (timer > 0) {
+//     timer--;
+//     console.log(timer);
+//   }
+// }, 1000);
