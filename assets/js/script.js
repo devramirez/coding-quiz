4f9864cc0cadff939263ed41131ab3b79927ac4e
devramirez <aramirezdev1@gmail.com>
2023-05-11 18:49:16 -0400

    added starting query selectors plus Q&A

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
-var count = 0;
-var startEl = document.querySelector('.start-btn');
-var countEl = document.querySelector('.count');
+// Intro Section
+var welcome = document.querySelector('#introduction');
+var introPage = document.querySelector('#intro-page');
+var startBtn = document.querySelector('#start-button');
+// Question Section
+var questionPage = document.querySelector('#question-page');
+var questionAsk = document.querySelector('#question-ask');
+// Answer Buttons
+var reactButtons = document.querySelector('.answer');
+var answerBtn1 = document.querySelector('#answer_btn1');
+var answerBtn2 = document.querySelector('#answer_btn2');
+var answerBtn3 = document.querySelector('#answer_btn3');
+var answerBtn4 = document.querySelector('#answer_btn4');
+
+
+
+
+
 
 // Function to countdown
-function countDown() {
-    var timer = 60;
-setInterval(() => {
-  document.getElementById("timer").textContent = timer;
-  if (timer > 0) {
-    timer--;
-    console.log(timer)
-  }
-}, 1000);
-}
-
-// setting a timer interval
-// var timer = 60;
+// function countDown() {
+//     var timer = 60;
 // setInterval(() => {
 //   document.getElementById("timer").textContent = timer;
 //   if (timer > 0) {
 //     timer--;
-//     console.log(timer);
+//     console.log(timer)
 //   }
 // }, 1000);
+// }
+
